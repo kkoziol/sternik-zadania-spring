06ac005665ede8441803aa70a8d6c8a418c0d5ed
Krzysiek Koziol <kkoziol@gmail.com>
2018-03-24 15:21:32 +0100

    Zad 8

--- src/main/java/pl/sternik/kk/zadania/spring/zad08/PrototypeBean.java
+++ src/main/java/pl/sternik/kk/zadania/spring/zad08/PrototypeBean.java
@@ -3,6 +3,8 @@ package pl.sternik.kk.zadania.spring.zad08;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Component
+@Scope("prototype")
 public class PrototypeBean {
 
 
--- src/main/java/pl/sternik/kk/zadania/spring/zad08/SingletonBean.java
+++ src/main/java/pl/sternik/kk/zadania/spring/zad08/SingletonBean.java
@@ -2,6 +2,7 @@ package pl.sternik.kk.zadania.spring.zad08;
 
 import org.springframework.stereotype.Component;
 
+@Component
 public class SingletonBean {
 
 
