90cacb025e625ff004d8c5648d3ec0d536048946
Krzysiek Koziol <kkoziol@gmail.com>
2018-03-24 12:45:19 +0100

    LEniwa adnotacja

--- src/main/java/pl/sternik/kk/zadania/spring/zad02/MySecondBean.java
+++ src/main/java/pl/sternik/kk/zadania/spring/zad02/MySecondBean.java
@@ -1,8 +1,10 @@
 package pl.sternik.kk.zadania.spring.zad02;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class MySecondBean {
 
 	private String name;
