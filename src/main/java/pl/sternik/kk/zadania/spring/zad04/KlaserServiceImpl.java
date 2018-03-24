38c82b46b81251ec7550416375d0d87d318ea1a1
Krzysiek Koziol <kkoziol@gmail.com>
2018-03-24 12:22:00 +0100

    poprawka import

--- src/main/java/pl/sternik/kk/zadania/spring/zad04/KlaserServiceImpl.java
+++ src/main/java/pl/sternik/kk/zadania/spring/zad04/KlaserServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import pl.sternik.kk.zadania.spring.zad05.DrugieDao;
+//import pl.sternik.kk.zadania.spring.zad05.DrugieDao;
 
 
 public class KlaserServiceImpl {
